5fd438954386bda777b80086a248d38273bf3b68
SikandarJODD <sikandar6174@gmail.com>
2026-08-12 04:07:39 +0530

    feat:new sidebar update

---
+++ src/lib/components/nav-main.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+	import HouseIcon from '@lucide/svelte/icons/house';
+	import InboxIcon from '@lucide/svelte/icons/inbox';
+	import SearchIcon from '@lucide/svelte/icons/search';
+	import SparklesIcon from '@lucide/svelte/icons/sparkles';
+	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+	import type { Component } from 'svelte';
+
+	type NavItem = {
+		title: string;
+		url: string;
+		icon: Component;
+		badge?: string;
+		isActive?: boolean;
+	};
+
+	let items: NavItem[] = [
+		{
+			title: 'Home',
+			url: '/',
+			icon: HouseIcon
+		},
+		{
+			title: 'Library',
+			url: '#',
+			icon: SparklesIcon
+		},
+		{
+			title: 'Components',
+			url: '#',
+			icon: HouseIcon,
+			isActive: true
+		}
+	];
+</script>
+
+<Sidebar.Group>
+	<Sidebar.GroupContent>
+		<Sidebar.Menu>
+			{#each items as item (item.title)}
+				<Sidebar.MenuItem>
+					<Sidebar.MenuButton>
+						{#snippet child({ props })}
+							<a href={item.url} {...props}>
+								<item.icon />
+								<span>{item.title}</span>
+							</a>
+						{/snippet}
+					</Sidebar.MenuButton>
+				</Sidebar.MenuItem>
+			{/each}
+		</Sidebar.Menu>
+	</Sidebar.GroupContent>
+</Sidebar.Group>
